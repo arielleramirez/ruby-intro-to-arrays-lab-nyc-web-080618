ae53ae1a971d7a673771e93b99768e9319078f00
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 16:13:54 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,10 +19,11 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array[-1]
+  array[0]
 end
 
 def last_element_with_array_methods(array)
+  array
 end
 
 def length_of_array(array)
